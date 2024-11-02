fe51c726cfe0b6f2c05ba5d996cc06d413cb9aee
Juan Manuel Caceres <juanmanuelcaceres97@gmail.com>
2024-11-02 18:49:40 -0300

    tabla productos + getProducts

--- src/app/modules/product/product/product.component.ts
+++ src/app/modules/product/product/product.component.ts
@@ -1,16 +1,53 @@
-import { Component, ViewChild } from '@angular/core';
+import { Component, inject, OnInit, ViewChild } from '@angular/core';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatTableDataSource } from '@angular/material/table';
+import { ProductService } from '../../shared/service/product.service';
 @Component({
   selector: 'app-product',
   templateUrl: './product.component.html',
   styleUrls: ['./product.component.css']
 })
-export class ProductComponent {
+export class ProductComponent implements OnInit{
+  
+  private productService = inject(ProductService);
+  
+  ngOnInit(): void {
+    this.getProducts();
+  }
   
   
   @ViewChild(MatPaginator)
   paginator!: MatPaginator;
+  
+  getProducts(){
+    this.productService.getProducts()
+      .subscribe({
+        next:(data)=>{
+          console.log("product response: ",data)
+          this.proccesProductResponse(data);
+        },
+        error:(error)=>{
+          console.log("error: ",error)
+        }
+      })
+  }
+  proccesProductResponse(resp: any) {
+    const dataProduct: ProductElement[]=[];
+
+    if(resp.metadata[0].code == "00"){
+      let listProduct = resp.productResponse.products;
+
+      listProduct.forEach((product:ProductElement)=>{
+        dataProduct.push(product);
+      });
+
+      this.dataSource = new MatTableDataSource<ProductElement>(dataProduct);
+      this.dataSource.paginator = this.paginator;
+    }
+  }
+
+  
+ 
   
   openCategoryDialog() {
     throw new Error('Method not implemented.');
@@ -18,6 +55,15 @@ export class ProductComponent {
   buscar(arg0: string) {
     throw new Error('Method not implemented.');
   }
+
+  editProduct(element : any){
+
+  }
+
+  deleteProduct(element:any){
+
+  }
+
 
   displayedColumns: string[]=['id','name','description','accounts','costPrice','sellPrice','picture','actions'];
   dataSource : MatTableDataSource<ProductElement> = new MatTableDataSource<ProductElement>();
@@ -30,5 +76,6 @@ export interface ProductElement{
   accounts: number;
   costPrice: number;
   sellPrice: number;
-  picture: any;
+  picture: string;
+
 }
